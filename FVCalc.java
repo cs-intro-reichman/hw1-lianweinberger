a0f3ac46b2eb4affce01bb5c04703fb32359892c
lianlevwe <lianlevwe@gmail.com>
2024-11-06 21:21:23 +0200

    finish task 1 and 2

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]); 	
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]); 	
+		double ratePow = Math.pow((1 + rate), n);
+		double futureValue = (currentValue * ratePow);
+		System.out.println((int) futureValue);
 	}
 }
