c4d1b043755bf02daab76c496f59ba9d62043cb7
lianlevwe <lianlevwe@gmail.com>
2024-11-06 21:30:45 +0200

    fix Bill3

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args[2];
 		int billAmount = Integer.parseInt(args[3]);
 		double eachAmount = Math.ceil((double) billAmount / 3);
-		System.out.println("Dear " + name1 + " " + name2 + ", and " + name3 + ": pay " + eachAmount + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1+ ": pay " + eachAmount + " Shekels each");
 
 	}
 }
