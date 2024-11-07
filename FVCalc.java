2d3977c3c384d79c1ebb7ff7a261dedcc1c4be0f
lianlevwe <lianlevwe@gmail.com>
2024-11-07 07:50:04 +0200

    fix FVClac.java

--- FVCalc.java
+++ FVCalc.java
@@ -3,7 +3,7 @@ public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
 		int currentValue = Integer.parseInt(args[0]); 	
-		double rate = Double.parseDouble(args[1]);
+		double rate = Double.parseDouble(args[1]) / 100;
 		int n = Integer.parseInt(args[2]); 	
 		double ratePow = Math.pow((1 + rate), n);
 		double futureValue = (currentValue * ratePow);
